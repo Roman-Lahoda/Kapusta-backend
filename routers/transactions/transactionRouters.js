d32f1ed4fa32c37b4dfaea7faab6be0fc20f2370
Roman-Lahoda <lagoda9515@gmail.com>
2022-02-05 12:36:17 +0200

    change files with prettierrc

--- routers/transactions/transactionRouters.js
+++ routers/transactions/transactionRouters.js
@@ -1,14 +1,13 @@
-import Router from 'express'
-import transactionModel from '../../model/transactionModel.js'
-import TransactionController from '../../controllers/transactions/transactionController.js'
-
+import Router from 'express';
+import transactionModel from '../../model/transactionModel.js';
+import TransactionController from '../../controllers/transactions/transactionController.js';
 
 const router = new Router();
 
-router.post ('/transactions', TransactionController.create )
-router.get ('/transactions', TransactionController.getAll)
-router.get ('/transactions/:id', TransactionController.getOne)
-router.put ('/transactions', TransactionController.update)
-router.delete ('/transactions/:id', TransactionController.delete)
+router.post('/transactions', TransactionController.create);
+router.get('/transactions', TransactionController.getAll);
+router.get('/transactions/:id', TransactionController.getOne);
+router.put('/transactions', TransactionController.update);
+router.delete('/transactions/:id', TransactionController.delete);
 
 export default router;
