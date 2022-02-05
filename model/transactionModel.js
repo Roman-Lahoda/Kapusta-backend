731d574f9a7de690d57f9387d952427ffa576759
RomanKorotaev <kro1986@ukr.net>
2022-02-05 12:30:27 +0200

    Добавлен npm i cross-env, .prettierrc.json  и подправлен скрипт start
    Также теперь в модели транзакции есть дата записи

--- model/transactionModel.js
+++ model/transactionModel.js
@@ -6,7 +6,8 @@ const transactionModel = new mongoose.Schema(
         sum: { type: Number, required: true},
         category: { type: String, required: true},
         destination: { type: String, required: true}
-    }
+        
+    }, {versionKey: false, timestamps: true}
 )
 
 export default mongoose.model ('transactions', transactionModel);
