96b494443d838743a8fd2abf62a8215e67b1d33d
Ihor Rudniev <rudnievihor@gmail.com>
2022-02-08 22:09:16 +0200

    add_login_users

--- controllers/authentication/authenticationController.js
+++ controllers/authentication/authenticationController.js
@@ -1,6 +1,8 @@
 // import { request } from 'https';
 import { HttpCode } from '../../constants/httpCode.js';
 import users from '../../repository/users.js';
+import JWT from 'jsonwebtoken';
+// const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 class AuthenticationService {
   async isUserExist(email) {
@@ -9,8 +11,28 @@ class AuthenticationService {
   }
 
   async create(body) {
-    const { id, name, email, owner } = await users.create(body);
-    return { id, name, email, owner };
+    const { id, name, email, balans, owner } = await users.create(body);
+    return { id, name, email, balans, owner };
+  }
+
+  async getUser(email, password) {
+    const user = await users.findByEmail(email);
+    const isValidPassword = await user?.isValidPassword(password);
+    if (!isValidPassword) {
+      return null;
+    }
+    return user;
+  }
+
+  getToken(user) {
+    const { id, email } = user.id;
+    const payload = { id, email };
+    const token = JWT.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '8h' });
+    return token;
+  }
+
+  async setToken(id, token) {
+    await users.updateToken(id, token);
   }
 }
 
@@ -21,14 +43,27 @@ const registration = async (req, res, next) => {
   const isUserExist = await authenticationService.isUserExist(email);
   if (isUserExist) {
     return res
-      .status(HttpCode.OK)
+      .status(HttpCode.CONFLICT)
       .json({ status: 'error', code: HttpCode.CONFLICT, message: 'Email is already exist' });
   }
+
   const userData = await authenticationService.create(req.body);
   res.status(HttpCode.OK).json({ status: 'success', code: HttpCode.OK, userData });
 };
 
-const login = (req, res, next) => {};
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await authenticationService.getUser(email, password);
+  if (!user) {
+    return res
+      .status(HttpCode.UNAUTHORIZED)
+      .json({ status: 'error', code: HttpCode.UNAUTHORIZED, message: 'Invalid credentials' });
+  }
+
+  const token = authenticationService.getToken(user);
+  await authenticationService.setToken(user.id, token);
+  res.status(HttpCode.OK).json({ status: 'success', code: HttpCode.OK, userData: { token } });
+};
 
 const logout = (req, res, next) => {};
 
