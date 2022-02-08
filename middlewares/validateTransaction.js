a1c2a468a140623417809804dc44ff1c9fa2ab61
RomanKorotaev <kro1986@ukr.net>
2022-02-08 14:00:19 +0200

    Add validation for categories

--- middlewares/validateTransaction.js
+++ middlewares/validateTransaction.js
@@ -1,23 +1,23 @@
 import Joi from 'joi'
 
 const createTransactionSchema = Joi.object({
-    transactionType: Joi.string().min(2).max(100).required(),
-    sum: Joi.number().min(0.01).required(),
-    category: Joi.string().min(2).max(100).required(),
+    transactionType: Joi.string().valid('income', 'expense').required(),
+    sum: Joi.number().min(1).integer().required(),
+    category: Joi.string().valid('transport', 'food', 'health', 'alcohol', 'entertainment', 'housing', 'technics', 'communal', 'sport', 'education', 'other' ).required(),
     destination: Joi.string().min(2).max(100).required(),
-    dayCreate: Joi.number().min(1).max(31).required(),
-    monthCreate: Joi.number().min(1).max(12).required(),
-    yearCreate: Joi.number().min(2018).max(2030).required(),
+    dayCreate: Joi.number().integer().min(1).max(31).required(),
+    monthCreate: Joi.number().integer().min(1).max(12).required(),
+    yearCreate: Joi.number().integer().min(2018).max(2030).required(),
 })
 
 const updateTransactionSchema = Joi.object({
-    transactionType: Joi.string().min(2).max(100).optional(),
-    sum: Joi.number().min(0.01).optional(),
-    category: Joi.string().optional(),
-    destination: Joi.string().optional(),
-    dayCreate: Joi.number().min(1).max(31).optional(),
-    monthCreate: Joi.number().min(1).max(12).optional(),
-    yearCreate: Joi.number().min(2018).max(2030).optional(),
+    transactionType: Joi.string().valid('income', 'expense').optional(),
+    sum: Joi.number().min(1).integer().optional(),
+    category: Joi.string().valid('transport', 'food', 'health', 'alcohol', 'entertainment', 'housing', 'technics', 'communal', 'sport', 'education', 'other' ).optional(),
+    destination: Joi.string().min(2).max(100).optional(),
+    dayCreate: Joi.number().integer().min(1).max(31).optional(),
+    monthCreate: Joi.number().integer().min(1).max(12).optional(),
+    yearCreate: Joi.number().integer().min(2018).max(2030).optional(),
 }).or('transactionType', 'sum', 'category', 'destination', 'dayCreate', 'monthCreate', 'yearCreate')
 
 
@@ -26,7 +26,7 @@ export const validateCreateTransaction = async (req, res, next) => {
         const value = await createTransactionSchema.validateAsync(req.body);
     }
     catch (err) { 
-        return res.status(400).json({message: `Field ${err.message.replace(/"/g, '')}` })
+           return res.status(400).json({message: `Field : ${err.message.replace(/"/g, '')}` })
     }
     next()
 }
@@ -37,7 +37,9 @@ export const validateUpdateTransaction = async (req, res, next) => {
         const value = await updateTransactionSchema.validateAsync(req.body);
     }
     catch (err) { 
-        return res.status(400).json({message: `Field ${err.message.replace(/"/g, '')}` })
+
+
+        return res.status(400).json({message: `Field : ${err.message.replace(/"/g, '')}` })
     }
     next()
 }
