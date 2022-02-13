668217f4a6346bf37fc084e234c5d5371a55530d
Ihor Rudniev <rudnievihor@gmail.com>
2022-02-13 19:30:06 +0200

    fix

--- controllers/authentication/authenticationGoogleController.js
+++ controllers/authentication/authenticationGoogleController.js
@@ -1,7 +1,13 @@
 import queryString from 'query-string';
 import axios from 'axios';
+import UserModel from '../../model/userModel.js';
+// import users from '../../repository/users.js';
+import bcrypt from 'bcryptjs/dist/bcrypt.js';
+// import { AuthenticationService } from '../authentication/authenticationController.js';
+
 // import URL from 'url';
 // import { query } from 'express';
+// const authenticationService = new AuthenticationService();
 
 const googleAuth = async (req, res) => {
   const stringifiedParams = queryString.stringify({
@@ -43,21 +49,58 @@ const googleRedirect = async (req, res) => {
     },
   });
 
-  /*
-   * Логика поиска юзера в нашей базе  по email
-   */
-  // по userData.data.email мы може обратиться к email и:
-  // ...если юзера в базе данных нет, то мы его регистрируем
-  // ...если юзер есть, то даем ему токен и пускаем в базу данных
-  // ...
-  // ...
-  // в query параметрах мы указываем токен
-  // return res.redirect(`${process.env.FRONTEND_URL}?email=${userData.data.email}`);
-  // console.log('userData.data.email', userData.data.email);
-  return res.redirect(
-    // `${process.env.FRONTEND_URL}/google-redirect/?accessToken=${accessToken}&refreshToken=${refreshToken}`,
-    `${process.env.FRONTEND_URL}?email=${userData.data.email}`,
-  );
+  // Логика поиска юзера в нашей базе  по email
+  const { name, email } = userData.data;
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    const hashPasword = bcrypt.hashSync(id, bcrypt.genSaltSync(10));
+    const userGoogle = await UserModel.create({
+      name: name,
+      email: email,
+      password: hashPasword,
+      token: null,
+      balanse: null,
+      verify: true,
+      verifyToken: null,
+    });
+    const { _id } = userGoogle;
+    const payload = {
+      id: _id,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY);
+    await UserModel.findByIdAndUpdate(_id, { token });
+    return res.redirect(`${FRONTEND_URL}?access_token=${token}`);
+  }
+  const { _id } = user;
+  const payload = {
+    id: _id,
+  };
+  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY);
+  await UserModel.findByIdAndUpdate(_id, { token });
+  return res.redirect(`${FRONTEND_URL}?access_token=${token}`);
 };
+
+// по userData.data.email мы може обратиться к email и:
+// const { email } = userData.data;
+// const userByGoogle = async email => {
+//   const user = users.findByEmail({ email });
+//   // ...если юзер есть, то даем ему токен и пускаем в базу данных
+//   if (!user) {
+//     // ...если юзера в базе данных нет, то мы его регистрируем
+//     const userData = await authenticationService.create(req.body);
+//   }
+//   const accessToken = authenticationService.getToken(user);
+//   await authenticationService.setToken(user.id, accessToken);
+// };
+
+// ...
+// ...
+// в query параметрах мы указываем токен
+// return res.redirect(`${process.env.FRONTEND_URL}?email=${userData.data.email}`);
+// console.log('userData.data.email', userData.data.email);
+// return res.redirect(
+//   `${process.env.FRONTEND_URL}/google-redirect/?accessToken=${accessToken}&refreshToken=${refreshToken}`,
+// `${process.env.FRONTEND_URL}?email=${userData.data.email}`,
+// );
 
 export default { googleAuth, googleRedirect };
